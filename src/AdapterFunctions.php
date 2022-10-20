e376581cfa458d6dc885a61e25f1f6cabd18c24d
Joanhey <joanhey@kumbiaphp.com>
2022-10-20 12:32:12 +0200

    Remove headers_sent()

--- src/AdapterFunctions.php
+++ src/AdapterFunctions.php
@@ -24,17 +24,6 @@ function header_remove(string $name): void
 }
 
 /**
- * Checks if or where headers have been sent
- * Always false with Workerman and CLI, Fix problem with cli SAPI
- * 
- * @return bool
- */
-function headers_sent($file = null, $line = null): bool
-{
-    return false;
-}
-
-/**
  * Get or Set the HTTP response code
  *
  * @param integer $code [optional] The optional response_code will set the response code.
--- src/Adapterman.php
+++ src/Adapterman.php
@@ -10,7 +10,7 @@ class Adapterman
 
     public const NAME = "Adapterman v". self::VERSION;
     
-    private const FUNCTIONS = ['header', 'header_remove', 'headers_sent', 'http_response_code', 'setcookie', 'session_create_id', 'session_id', 'session_name', 'session_save_path', 'session_status', 'session_start', 'session_write_close', 'set_time_limit'];
+    private const FUNCTIONS = ['header', 'header_remove', 'http_response_code', 'setcookie', 'session_create_id', 'session_id', 'session_name', 'session_save_path', 'session_status', 'session_start', 'session_write_close', 'set_time_limit'];
 
     public static function init(): void
     {
