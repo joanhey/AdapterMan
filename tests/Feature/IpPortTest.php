0f8ad2df5a95ddae039bf19752dad6174a7647c9
Joanhey <joanhey@kumbiaphp.com>
2023-06-28 13:19:09 +0200

    Add $_SERVER['SERVER_ADDR'] to tests

--- tests/Feature/IpPortTest.php
+++ tests/Feature/IpPortTest.php
@@ -7,10 +7,10 @@ it('tests client IP', function () {
         ->toBe('127.0.0.1');
 });
 
-// Searching the best solution
+
 it('tests server IP', function () {
     $response = HttpClient()->get('/server_ip');
 
     expect($response->getBody()->getContents())
         ->toBe('127.0.0.1');
-})->todo();
+});
