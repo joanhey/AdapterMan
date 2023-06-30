2e6bb2ed5ed67376983d2d3120b4b03e51bdd30e
Joanhey <joanhey@kumbiaphp.com>
2023-06-30 23:11:33 +0200

    Clean AdaptermanServer in tests

--- tests/AdaptermanServer.php
+++ tests/AdaptermanServer.php
@@ -13,21 +13,24 @@ $worker->name  = "Adapterman Tests";
 $worker->count = 2;
 
 $worker->onMessage = static function (TcpConnection $connection, $request) {
-    match (parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)) {
-        '/' => $connection->send('Hello World!'),
-        '/get' => $connection->send(json_encode($_GET)),
-        '/post' => $connection->send(json_encode($_POST)),
-        '/headers' => $connection->send(json_encode(getallheaders())),
-        '/method' => $connection->send($_SERVER['REQUEST_METHOD']),
-        '/server_ip' => $connection->send($_SERVER['SERVER_ADDR']),
-        '/ip' => $connection->send($_SERVER['REMOTE_ADDR']),
-        '/file' => $connection->send(json_encode($request->file('file'))),
 
-        default => (function () use ($connection) {
+    $response = match (parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)) {
+        '/' => 'Hello World!',
+        '/get' => json_encode($_GET),
+        '/post' => json_encode($_POST),
+        '/headers' => json_encode(getallheaders()),
+        '/method' => $_SERVER['REQUEST_METHOD'],
+        '/server_ip' => $_SERVER['SERVER_ADDR'],
+        '/ip' => $_SERVER['REMOTE_ADDR'],
+        '/upload' => json_encode($_FILES),
+
+        default => (function () {
             http_response_code(404);
-            $connection->send('404 Not Found');
+            return '404 Not Found';
         })(),
     };
+
+    $connection->send($response);
 };
 
 Worker::runAll();
