58edd54bdf56ec4d9eae11de0c730f806c544e49
Joanhey <joanhey@kumbiaphp.com>
2023-07-01 20:54:26 +0200

    Fix test to send POST with raw data

--- tests/Feature/ConvertExtVarToUnderscoreTest.php
+++ tests/Feature/ConvertExtVarToUnderscoreTest.php
@@ -12,19 +12,24 @@ it('converts GET Query var name to underscore', function (string $name, string $
 })->with('ext vars to underscore');
 
 
-it('convert POST var name to underscore', function (string $name, string $converted) {
+it('convert POST "application/x-www-form-urlencoded" var name to underscore', function (string $name, string $converted) {
     $response = HttpClient()->post('/post', [
-        'form_params' => [$name, 'test']
+        //'form_params' => [],
+        // force send raw data
+        'curl' => [
+            CURLOPT_HTTPHEADER  => ["application/x-www-form-urlencoded"],
+            CURLOPT_POSTFIELDS  => "$name=test",
+        ]
     ]);
 
     expect($response->getBody()->getContents())
         ->toBeJson()
         ->json()
-        ->toBe([$converted, 'test']);
-})->with('ext vars to underscore')
-->todo();
+        ->toBe([$converted => 'test']);
+})->with('ext vars to underscore');
 
-it('convert POST multipart var name to underscore', function (string $name, string $converted) {
+
+it('convert POST "multipart/form-data" var name to underscore', function (string $name, string $converted) {
     $response = HttpClient()->post('/post', [
         'multipart' => [[
             'name'     => $name,
