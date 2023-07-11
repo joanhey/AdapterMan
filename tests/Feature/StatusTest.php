70a1e9517998d0af83b0648cf3f9c3beaffc1ca6
Joanhey <joanhey@kumbiaphp.com>
2023-07-11 16:54:56 +0200

    Update StatusTest

--- tests/Feature/StatusTest.php
+++ tests/Feature/StatusTest.php
@@ -1,12 +1,19 @@
 <?php
 
+it('tests 200 GET', function () {
+    $response = HttpClient()->get('/');
+
+    expect($response->getStatusCode())
+        ->toBe(200);
+});
+
+
 it('tests 404 GET', function () {
     $response = HttpClient()->get('/404');
-    
+
     expect($response->getStatusCode())
         ->toBe(404)
         ->and($response->getBody()->getContents())
         ->toBe('404 Not Found');
 });
-
 
