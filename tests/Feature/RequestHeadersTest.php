6a0014e71d031fb5c926c7c852d0d26047377ef0
Joanhey <joanhey@kumbiaphp.com>
2023-07-11 16:03:27 +0200

    Add more headers tests

--- tests/Feature/RequestHeadersTest.php
+++ tests/Feature/RequestHeadersTest.php
@@ -29,3 +29,24 @@ it('get Request Headers', function (array $data) {
         ->toMatchArray($data);
 
 })->with('REQUEST HEADERS');
+
+it('get headers', function (string $key, string $value) {
+    $response = HttpClient()->get('/headers');
+    
+    $content = $response->getBody()->getContents();
+    expect($content)
+        ->toBeJson()
+        ->json()
+        ->toBeArray();
+
+    // convert all keys to lowercase in validation
+    // header names are case insensitive according to the HTTP specification
+    $content = array_change_key_case(json_decode($content, true));
+    expect($content)
+        ->{strtolower($key)}
+        ->toBe($value);
+
+})->with([
+    ['Host', '127.0.0.1:8080'],
+    ['User-Agent', 'Testing/1.0']
+]);
