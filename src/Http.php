881bb5b4239ddefb5d9d13701c3870dc2a85030d
Joanhey <joanhey@kumbiaphp.com>
2023-07-13 01:58:24 +0200

    Enforce copyright in the files As some devs are by passing the copyright

--- src/Http.php
+++ src/Http.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This file is part of Adapterman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    Joan Miquel<https://github.com/joanhey>
+ * @copyright Joan Miquel<https://github.com/joanhey>
+ * @link      https://github.com/joanhey/AdapterMan
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 
 namespace Adapterman;
 
