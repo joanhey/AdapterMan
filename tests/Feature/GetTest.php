c35a3ca2ddc3d3d7c6cf538d7fddeb22978d054b
Joanhey <joanhey@kumbiaphp.com>
2023-07-13 13:04:49 +0200

    Add complex array to the Query string test

--- tests/Feature/GetTest.php
+++ tests/Feature/GetTest.php
@@ -25,3 +25,28 @@ it('tests GET Query string', function (array $data) {
         ->toBe($data);
 })->with('send data');
 
+
+it('tests GET Query string with complex array', function (array $data) {
+    $response = HttpClient()->get('/get', [
+        'query' => $data,
+    ]);
+
+    expect($response->getBody()->getContents())
+        ->toBeJson()
+        ->json()
+        ->toBe($data);
+})->with([['complex array' => [
+    'user' => [
+        'name' => 'Bob Smith',
+        'age'  => '47',
+        'sex'  => 'M',
+        'dob'  => '5/12/1956',
+    ],
+    'pastimes' => ['golf', 'opera', 'poker', 'rap'],
+    'children' => [
+        'bobby' => ['age'=>'12', 'sex'=>'M'],
+        'sally' => ['age'=>'8', 'sex'=>'F'],
+    ],
+    'CEO',
+    ],]
+]);
